4201a94ca210d815dd7b6b6ef68540a75b7a2959
Red Ochsenbein <red@redochsenbein.ch>
2019-03-22 17:43:13 +0100

    Refactor the Configuration component

--- src/lib/components/Configuration.js
+++ src/lib/components/Configuration.js
@@ -1,120 +1,118 @@
-import React, { Component } from 'react';
+import React, { memo, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { setConfig } from '../actions';
 
-class Configuration extends Component {
-    state = { defaultsLoaded: false };
+const Configuration = (props) => {
+  const setConfig = (key, value) => {
+    props.setConfig(key, value);
+    if (typeof(Storage) !== "undefined") {
+      const userId = props.user.id;
+      window.localStorage.setItem(userId+'.'+key, value);
+    }
+  }
 
-    componentDidUpdate() {
-        this.loadDefaults();
+  // TODO: This should be loaded further up the chain
+  useEffect(() => {
+    if (!props.user) {
+        return;
     }
 
-    loadDefaults() {
-        if (this.state.defaultsLoaded || !this.props.user) {
-            return;
-        }
-        if (typeof(Storage) !== "undefined") {
-            const userId = this.props.user.id;
-            [
-                'amountType',
-                'randomListName',
-                'trackCount',
-                'trackMinutes',
-                'purgeOnShuffle',
-            ].forEach(key => {
-                const mapping = key => {
-                    switch (key) {
-                        case 'purgeOnShuffle':
-                            return window.localStorage.getItem(userId+'.'+key) == 'true';
-                        default:
-                            return window.localStorage.getItem(userId+'.'+key);
-                    }
-                };
-                const value = mapping(key);
-                if (value !== null) {
-                    this.setConfig(key, value);
+    if (typeof(Storage) !== "undefined") {
+        const userId = props.user.id;
+        [
+            'amountType',
+            'randomListName',
+            'trackCount',
+            'trackMinutes',
+            'purgeOnShuffle',
+        ].forEach(key => {
+            const mapping = key => {
+                switch (key) {
+                    case 'purgeOnShuffle':
+                        return window.localStorage.getItem(userId+'.'+key) == 'true';
+                    default:
+                        return window.localStorage.getItem(userId+'.'+key);
                 }
-            });
-        }
-        this.setState({defaultsLoaded: true});
+            };
+            const value = mapping(key);
+            if (value !== null) {
+              setConfig(key, value);
+            }
+        });
     }
+  }, [props.user]);
 
-    setConfig = (key, value) => {
-        this.props.setConfig(key, value);
-        if (typeof(Storage) !== "undefined") {
-            const userId = this.props.user.id;
-            window.localStorage.setItem(userId+'.'+key, value);
-        }
-    }
+  const handleChange = event => {
+    const value = parseInt(event.target.value) || '';
+    const key = props.config.amountType == 'minutes'
+      ? 'trackMinutes' : 'trackCount';
+    const maxValue = props.config.amountType == 'minutes'
+      ? 1500 : 500;
+    setConfig(key, Math.max(Math.min(value, maxValue), 0));
+  };
 
-    handlePurgeChange = _ => {
-        this.setConfig('purgeOnShuffle', !this.props.config.purgeOnShuffle);
-    }
+  const handlePurgeChange = _ => {
+    setConfig('purgeOnShuffle', !props.config.purgeOnShuffle);
+  }
 
-    handleChange = event => {
-        const value = parseInt(event.target.value) || '';
-        const key = this.props.config.amountType == 'minutes'
-            ? 'trackMinutes' : 'trackCount';
-        const maxValue = this.props.config.amountType == 'minutes'
-            ? 1500 : 500;
-        this.setConfig(key, Math.max(Math.min(value, maxValue), 0));
-    }
+  const handlePlaylistNameChange = event => {
+    const input = event.target.value.trim();
+    const value = input || 'Advanced Shuffle';
+    setConfig('randomListName', value);
+  }
 
-    handlePlaylistNameChange = event => {
-        const input = event.target.value.trim();
-        const value = input || 'Advanced Shuffle';
-        this.setConfig('randomListName', value);
-    }
+  const type = props.config.amountType == 'minutes' ? 'Minutes' : 'Trackcount';
+  const value = props.config.amountType == 'minutes'
+      ? props.config.trackMinutes
+      : props.config.trackCount;
+  const sentence = props.config.amountType == 'minutes' ? 'hours of music.' : 'random tracks.';
+  const sentenceValue = props.config.amountType == 'minutes'
+      ? Math.round(value / 60 * 100) / 100
+      : value;
+  const purgeSentence = props.config.purgeOnShuffle
+      ? "An existing playlist will be purged before adding new tracks."
+      : "The tracks will be prepended if there is an existing playlist.";
 
-    render() {
-        if (!this.props.showConfig) {
-            return "";
-        }
 
-        const type = this.props.config.amountType == 'minutes' ? 'Minutes' : 'Trackcount';
-        const value = this.props.config.amountType == 'minutes'
-            ? this.props.config.trackMinutes
-            : this.props.config.trackCount;
-        const sentence = this.props.config.amountType == 'minutes' ? 'hours of music.' : 'random tracks.';
-        const sentenceValue = this.props.config.amountType == 'minutes'
-            ? Math.round(value / 60 * 100) / 100
-            : value;
-        const purgeSentence = this.props.config.purgeOnShuffle
-            ? "An existing playlist will be purged before adding new tracks."
-            : "The tracks will be prepended if there is an existing playlist.";
+  if (!props.showConfig) {
+    return "";
+  }
 
-        return (
-            <div className="pt-2 mt-2 border-top border-bottom">
-                <div className="input-group mb-1 ">
-                    <div className="input-group-prepend">
-                        <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{type}</button>
-                        <div className="dropdown-menu">
-                            <a className="dropdown-item" href="#" onClick={_ => this.setConfig('amountType', 'count')}>Trackcount</a>
-                            <a className="dropdown-item" href="#" onClick={_ => this.setConfig('amountType', 'minutes')}>Minutes</a>
-                        </div>
-                    </div>
-                    <input type="text" className="form-control" onChange={this.handleChange} aria-label="Text input with dropdown button" value={value}/>
-                </div>
-                <small id="shuffleTypeHelp" className="mt-0 mb-3 form-text text-muted">The playlist will be filled with {sentenceValue} {sentence}</small>
-
-                <div className="form-group form-check custom-switch">
-                    <input type="checkbox" className="custom-control-input" id="purgeOnShuffle" onChange={this.handlePurgeChange} checked={this.props.config.purgeOnShuffle} />
-                    <label className="custom-control-label" htmlFor="purgeOnShuffle">Purge playlist</label>
-                    <small id="purgeHelp" className="form-text text-muted">{purgeSentence}</small>
-                </div>
-
-                <div className="form-group">
-                    <label htmlFor="playlistName">Playlist name</label>
-                    <input type="email" className="form-control" id="playlistName" aria-describedby="playlistNameHelp" placeholder="Playlist name" onChange={this.handlePlaylistNameChange} value={this.props.config.randomListName} />
-                    <small id="playlistNameHelp" className="form-text text-muted">The random playlist will be called "{this.props.config.randomListName}".</small>
+  return (
+    <div className="pt-2 mt-2 border-top border-bottom">
+        <div className="input-group mb-1 ">
+            <div className="input-group-prepend">
+                <button className="btn btn-outline-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">{type}</button>
+                <div className="dropdown-menu">
+                    <a className="dropdown-item" href="#" onClick={_ => setConfig('amountType', 'count')}>Trackcount</a>
+                    <a className="dropdown-item" href="#" onClick={_ => setConfig('amountType', 'minutes')}>Minutes</a>
                 </div>
             </div>
-        );
-    }
+            <input type="text" className="form-control" onChange={handleChange} aria-label="Text input with dropdown button" value={value}/>
+        </div>
+        <small id="shuffleTypeHelp" className="mt-0 mb-3 form-text text-muted">The playlist will be filled with {sentenceValue} {sentence}</small>
+
+        <div className="form-group form-check custom-switch">
+            <input type="checkbox" className="custom-control-input" id="purgeOnShuffle" onChange={handlePurgeChange} checked={props.config.purgeOnShuffle} />
+            <label className="custom-control-label" htmlFor="purgeOnShuffle">Purge playlist</label>
+            <small id="purgeHelp" className="form-text text-muted">{purgeSentence}</small>
+        </div>
+
+        <div className="form-group">
+            <label htmlFor="playlistName">Playlist name</label>
+            <input type="email" className="form-control" id="playlistName" aria-describedby="playlistNameHelp" placeholder="Playlist name" onChange={handlePlaylistNameChange} value={props.config.randomListName} />
+            <small id="playlistNameHelp" className="form-text text-muted">The random playlist will be called "{props.config.randomListName}".</small>
+        </div>
+    </div>
+  );
 }
 
 const mapStateToProps = ({data}) => {
-    return data;
+    return {
+      config: data.config,
+      showConfig: data.showConfig,
+      user: data.user,
+    };
 }
 
-export default connect(mapStateToProps, { setConfig })(Configuration);
+export default connect(mapStateToProps, { setConfig })(memo(Configuration));
