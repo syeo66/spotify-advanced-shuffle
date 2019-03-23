6ba85388e4b0b9681236e46d5305106d0f609cf0
Red Ochsenbein <red@redochsenbein.ch>
2019-03-23 15:06:04 +0100

    Refactored Player and PlayerInfo

--- src/lib/components/Player.js
+++ src/lib/components/Player.js
@@ -1,121 +1,107 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { retrievePlayState } from '../actions';
 import analyze from 'rgbaster';
 import Color from 'color';
 
-class Player extends Component {
-    oldImage = null;
+const Player = props => {
+  const [primaryColor, setPrimaryColor] = useState("rgb(30,30,30)");
+  const [secondaryColor, setSecondaryColor] = useState("rgb(200,200,200)");
+  const [tertiaryColor, setTertiaryColor] = useState("rgb(190,190,190)");
 
-    constructor (props) {
-      super(props);
-      this.state = {
-        color: "rgb(30,30,30)",
-        secondary: "rgb(200,200,200)",
-        tertiary: "rgb(190,190,190)",
-      }
+  useEffect(() => {
+    props.retrievePlayState(props.authenticated);
+    const polling = setInterval(_ => props.retrievePlayState(props.authenticated), 2000 + Math.floor(Math.random() * 1000));
+    return () => clearInterval(polling);
+  }, []);
+
+  useEffect(() => {
+    const item = props.playstate.item;
+    if (
+      item
+      && item.album
+      && item.album.images[0]
+      ) {
+        analyze(item.album.images[0].url, {
+          scale: 0.6,
+        }).then(colors => {
+          const primary = colors[0].color;
+
+          const secondary = colors.reduce((acc, c) =>  {
+            if (acc === null
+              && c.color !== primary.color
+              && Color(c.color).chroma() > 20
+              && Color(primary).contrast(Color(c.color)) > 4
+              ) {
+              return c;
+            }
+            return acc;
+          }, null);
+
+          const tertiary = colors.reduce((acc, c) =>  {
+            if (acc === null
+              && c.color !== primary.color
+              && Color('#fff').contrast(Color(c.color)) > 1
+              && Color('#000').contrast(Color(c.color)) > 1
+              && Color(primary).contrast(Color(c.color)) > 10) {
+              return c;
+            }
+            return acc;
+          }, null);
+
+          setPrimaryColor(colors[0].color);
+          setSecondaryColor(secondary ? secondary.color : tertiary
+            ? tertiary.color
+            : (Color(primary).isLight() ? 'rgb(30,30,30)' : 'rgb(240,240,240)'));
+          setTertiaryColor(tertiary ? tertiary.color :
+            secondary ? (Color(primary).isLight()
+              ? Color(secondary.color).darken(.3).hex() : Color(secondary.color).lighten(.3).hex())
+              : Color(primary).isLight() ? 'rgb(30,30,30)' : 'rgb(240,240,240)');
+        });
     }
+  }, [props.playstate.item ? props.playstate.item.album.images[0].url : null]);
 
-    componentDidMount() {
-      this.props.retrievePlayState(this.props.authenticated);
-      this.polling = setInterval(_ => this.props.retrievePlayState(this.props.authenticated), 2941);
-    }
+  const item = props.playstate.item;
+  const played = item && item.duration_ms ? Math.round(100 * props.playstate.progress_ms / item.duration_ms) : 0;
+  const image = item && item.album.images[0] ? {
+      backgroundImage: "linear-gradient(to right, rgba(255,255,255,.1) 50%, rgba(255,255,255,0)), url(" + item.album.images[0].url + ")",
+      transition: "background 5s linear",
+      backgroundSize: "cover",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "50% " + played + "%",
+    } : {};
+  const playClass = props.playstate.is_playing ? "fas fa-pause" : "fas fa-play";
+  const itemName = item && item.name ? item.name : '';
+  const artistName = item && item.artists[0].name ? item.artists[0].name : '';
+  const albumName = item && item.album.name ? item.album.name : '';
 
-    componentWillUnmount() {
-      clearInterval(this.polling);
-      this.polling = null;
-    }
-
-    componentWillReceiveProps(nextProps) {
-      const item = nextProps.playstate.item;
-      if (
-        item
-        && item.album
-        && item.album.images[0]
-        && item.album.images[0].url !== this.oldImage
-        ) {
-          analyze(item.album.images[0].url, {
-            scale: 0.6,
-          }).then(colors => {
-            const primary = colors[0].color;
-            const secondary = colors.reduce((acc, c) =>  {
-              if (acc === null
-                && c.color !== primary.color
-                && Color(c.color).chroma() > 20
-                && Color(primary).contrast(Color(c.color)) > 4
-                ) {
-                return c;
-              }
-              return acc;
-            }, null);
-            const tertiary = colors.reduce((acc, c) =>  {
-              if (acc === null
-                && c.color !== primary.color
-                && Color('#fff').contrast(Color(c.color)) > 1
-                && Color('#000').contrast(Color(c.color)) > 1
-                && Color(primary).contrast(Color(c.color)) > 10) {
-                return c;
-              }
-              return acc;
-            }, null);
-            return this.setState({
-              color: colors[0].color,
-              secondary: secondary ? secondary.color : tertiary
-                ? tertiary.color
-                : (Color(primary).isLight() ? 'rgb(30,30,30)' : 'rgb(240,240,240)'),
-              tertiary: tertiary ? tertiary.color :
-                secondary ? (Color(primary).isLight()
-                  ? Color(secondary.color).darken(.3).hex() : Color(secondary.color).lighten(.3).hex())
-                  : Color(primary).isLight() ? 'rgb(30,30,30)' : 'rgb(240,240,240)',
-            })
-          });
-          this.oldImage = item.album.images[0].url;
-      }
-    }
-
-    render() {
-      const item = this.props.playstate.item;
-      const played = item && item.duration_ms ? Math.round(100 * this.props.playstate.progress_ms / item.duration_ms) : 0;
-      const image = item && item.album.images[0] ? {
-          backgroundImage: "linear-gradient(to right, rgba(255,255,255,.1) 50%, rgba(255,255,255,0)), url(" + item.album.images[0].url + ")",
-          transition: "background 5s linear",
-          backgroundSize: "cover",
-          backgroundRepeat: "no-repeat",
-          backgroundPosition: "50% " + played + "%",
-        } : {};
-      const playClass = this.props.playstate.is_playing ? "fas fa-pause" : "fas fa-play";
-      const itemName = item && item.name ? item.name : '';
-      const artistName = item && item.artists[0].name ? item.artists[0].name : '';
-      const albumName = item && item.album.name ? item.album.name : '';
-
-      return (
-        <div className="my-3 border shadow rounded p-3 player" style={image}>
-          <div className="d-flex flex-row mb-2">
-            <button className="btn btn-primary mr-2 player__player-button" style={{
-              color: this.state.secondary,
-              backgroundColor: this.state.color,
-              border: "1px solid " + this.state.color,
-              boxShadow: "0 0 5px " + this.state.secondary,
-            }}><i className={playClass}></i></button>
-            <div className="" style={{color: this.state.tertiary}}>
-              <small style={{
-                textShadow:"0 0 10px " + this.state.color + ", 0 0 5px " + this.state.color + ', 0 0 3px ' + this.state.color,
-              }}><strong>{itemName}</strong><br />
-              {artistName} - {albumName}</small>
-            </div>
-          </div>
-          <div className="progress player__player-progress" style={{border: "1px solid " + this.state.color, boxShadow: "0 0 5px " + this.state.secondary, backgroundColor: this.state.secondary}}>
-            <div className="progress-bar" role="progressbar" style={{width: played + "%", backgroundColor: this.state.color}} aria-valuenow={played} aria-valuemin="0" aria-valuemax={100}></div>
-          </div>
+  return (
+    <div className="my-3 border shadow rounded p-3 player" style={image}>
+      <div className="d-flex flex-row mb-2">
+        <button className="btn btn-primary mr-2 player__player-button" style={{
+          color: secondaryColor,
+          backgroundColor: primaryColor,
+          border: "1px solid " + primaryColor,
+          boxShadow: "0 0 5px " + secondaryColor,
+        }}><i className={playClass}></i></button>
+        <div className="" style={{color: tertiaryColor}}>
+          <small style={{
+            textShadow:"0 0 10px " + primaryColor + ", 0 0 5px " + primaryColor + ', 0 0 3px ' + primaryColor,
+          }}><strong>{itemName}</strong><br />
+          {artistName} - {albumName}</small>
         </div>
-      );
-    }
+      </div>
+      <div className="progress player__player-progress" style={{border: "1px solid " + primaryColor, boxShadow: "0 0 5px " + secondaryColor, backgroundColor: secondaryColor}}>
+        <div className="progress-bar" role="progressbar" style={{width: played + "%", backgroundColor: primaryColor}} aria-valuenow={played} aria-valuemin="0" aria-valuemax={100}></div>
+      </div>
+    </div>
+  );
 }
 
-function mapStateToProps(state) {
+function mapStateToProps({auth, data}) {
   return {
-    authenticated: state.auth,
-    playstate: state.data.playstate ? state.data.playstate : [],
+    authenticated: auth,
+    playstate: data.playstate ? data.playstate : [],
   };
 }
 
--- src/lib/components/PlayerInfo.js
+++ src/lib/components/PlayerInfo.js
@@ -1,66 +1,60 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { retrievePlayerInfo } from '../actions';
 
-class PlayerInfo extends Component {
-    componentDidMount() {
-        this.props.retrievePlayerInfo(this.props.authenticated);
-        this.choosePlayer = this.choosePlayer.bind(this);
-        this.polling = setInterval(_ => this.props.retrievePlayerInfo(this.props.authenticated), 3000);
-    }
+const PlayerInfo = props => {
+  useEffect(() => {
+    props.retrievePlayerInfo(props.authenticated);
+    const polling = setInterval(_ => props.retrievePlayerInfo(props.authenticated), 3000);
+    return () => clearInterval(polling);
+  }, []);
 
-    componentWillUnmount() {
-        clearInterval(this.polling);
-        this.polling = null;
-    }
+  if (!props.devices || !props.devices.length) {
+    return (
+      <div className="alert alert-danger my-3 shadow" role="alert">
+        Sorry, could not find any devices! Please make sure Spotify is running.
+      </div>
+    );
+  }
 
-    choosePlayer(id) {
-        const authenticated = this.props.authenticated;
-        const url = "https://api.spotify.com/v1/me/player";
-        fetch(url, {
-            method: 'put', 
-            headers: new Headers({
-                'Authorization': 'Bearer '+authenticated
-            }),
-            body: JSON.stringify({
+  const devices = props.devices
+    .sort((a,b) => {return a.name.toUpperCase()<b.name.toUpperCase() ? -1 : 1})
+    .map(entry => {
+        const classe = "list-group-item list-group-item-action";
+        const activeClass = classe + (entry.is_active ? " active" : "");
+
+        const choosePlayer = _ => {
+          const id = entry.id;
+          const authenticated = props.authenticated;
+          const url = "https://api.spotify.com/v1/me/player";
+
+          fetch(url, {
+              method: 'put',
+              headers: new Headers({
+                'Authorization': 'Bearer ' + authenticated
+              }),
+              body: JSON.stringify({
                 device_ids: [id],
+              })
             })
-        })
-        .then(_ => {});
-    }
-
-    render() {
-        if (!this.props.devices || !this.props.devices.length) {
-            return (
-                <div className="alert alert-danger my-3 shadow" role="alert">
-                    Sorry, could not find any devices! Please make sure Spotify is running.
-                </div>
-            );
+            .then(_ => props.retrievePlayerInfo(props.authenticated));
         }
 
-        const devices = this.props.devices
-            .sort((a,b) => {return a.name.toUpperCase()<b.name.toUpperCase() ? -1 : 1})
-            .map(entry => {
-                    const classe = "list-group-item list-group-item-action";
-                    const activeClass = classe + (entry.is_active ? " active" : "");
-                    return (
-                        <a className={activeClass} href="#" onClick={_ => this.choosePlayer(entry.id)} key={entry.id}>{entry.name}</a>
-                    );
-                });
-
         return (
-            <div className="list-group my-3 shadow rounded">
-                {devices}
-            </div>
+          <a className={activeClass} href="#" onClick={choosePlayer} key={entry.id}>{entry.name}</a>
         );
-    }
+      });
+
+  return (
+    <div className="list-group my-3 shadow rounded">{devices}</div>
+  );
 }
 
-function mapStateToProps(state) {
-    return {
-        authenticated: state.auth,
-        devices: state.data.devices ? state.data.devices : [],
-    };
+function mapStateToProps({ auth, data }) {
+  return {
+    authenticated: auth,
+    devices: data.devices ? data.devices : [],
+  };
 }
 
 export default connect(mapStateToProps, { retrievePlayerInfo })(PlayerInfo);
