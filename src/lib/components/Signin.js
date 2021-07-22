1d2b592990fc062f641b345f17737e1956cbcef0
Red Ochsenbein <red@redochsenbein.ch>
2021-07-22 19:24:47 +0200

    Add storage message to login screen

--- src/lib/components/Signin.js
+++ src/lib/components/Signin.js
@@ -14,14 +14,21 @@ const Signin = ({ history }) => {
   }, [auth, history]);
 
   return (
-    <div className="row justify-content-center py-5">
-      <div className="col-auto py-5 shadow border rounded">
-        <a href="#" className="btn btn-success" onClick={signInWithSpotify}>
-          <i className="fab fa-spotify" />
-          &nbsp;Sign In With Spotify
-        </a>
+    <>
+      <div className="row justify-content-center py-5">
+        <div className="col-auto py-5 shadow border rounded">
+          <a href="#" className="btn btn-success" onClick={signInWithSpotify}>
+            <i className="fab fa-spotify" />
+            &nbsp;Sign In With Spotify
+          </a>
+        </div>
       </div>
-    </div>
+      <div className="row justify-content-center text-muted">
+        <div>
+          This website is communicating with the Spotify API only. It never sends or stores any data to its own server.
+        </div>
+      </div>
+    </>
   );
 };
 
